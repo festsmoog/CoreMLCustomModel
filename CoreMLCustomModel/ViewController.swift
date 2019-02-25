83d89512b67354af818c01b9e82ec619faa2cce8
Valera Petin <petinfest@yandex.ua>
2019-02-26 00:29:09 +0200

    Initial Commit

---
+++ CoreMLCustomModel/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  CoreMLCustomModel
+//
+//  Created by Petin Valerii on 20/6/18.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imgGuess: UIImageView!
+    @IBOutlet var lblGuess: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        
+        if let pickedImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
+            lblGuess.text = "Думаю..."
+            
+            // Устанавливаем изображение
+            imgGuess.contentMode = .scaleToFill
+            imgGuess.image = pickedImage
+            
+            // Получаем модель
+            guard let model = try? VNCoreMLModel(for: animals().model) else {
+                fatalError("Unable to load model")
+            }
+            
+            // Создаём запрос Vision
+            let request = VNCoreMLRequest(model: model) {[weak self] request, error in
+                guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("Unexpected results")
+                }
+                
+                // Обновляем UI с нашим результатом
+                DispatchQueue.main.async {[weak self] in
+                    self?.lblGuess.text = "\(topResult.identifier) с \(Int(topResult.confidence * 100))% вероятностью"
+                }
+            }
+            
+            guard let ciImage = CIImage(image: pickedImage)
+            else { fatalError("Cannot read picked image")}
+            
+            // Запускаем классификатор
+            let handler = VNImageRequestHandler(ciImage: ciImage)
+            DispatchQueue.global().async {
+                do {
+                    try handler.perform([request])
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    //Выбрать фото с галереи
+    @IBAction func takePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    //Сделать фото с камеры
+    @IBAction func takePhotoFromCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+}
+
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
